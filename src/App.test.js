7f5cdf556586233099277d4b3988af693f62080e
Savy Brandt <savybrandt@Savys-Air.attlocal.net>
2021-07-23 16:06:23 -0400

    test: App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios-hooks');
+jest.mock('./helpers');
+
+const helpers = require('./helpers');
+const useAxios = require('axios-hooks');
+
+describe('App', () => {
+  const questions = [
+    { category: 'foo', question: 'foo&bar', answer: 'bar' },
+  ];
+  
+  const errorPayload = [{ data: undefined, loading: false, error: 'oops!' }]
+  const loadingPayload = [{ data: undefined, loading: true, error: undefined }]
+  const resolvedPayload = [{ data: {results: [questions]}, loading: false, error: undefined }]
+
+  test('makes a call to the questions endpoint', () => {
+    useAxios.default.mockReturnValue(loadingPayload);
+    render(<App />);
+    expect(useAxios.default).toHaveBeenCalled();
+  });
+
+  test('renders "Loading..." when payload is not yet returned', () => {
+    useAxios.default.mockReturnValue(loadingPayload);
+    render(<App />);
+    const loadingElement = screen.getByText('Loading...');
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  test('throws an error if present', () => {
+    useAxios.default.mockReturnValue(errorPayload);
+    expect(() => render(<App />)).toThrow('oops!')
+  });
+
+  test('renders the IntroPage if data is present', () => {
+    useAxios.default.mockReturnValue(resolvedPayload);
+    helpers.reduceQuestionsPayload.mockReturnValue(questions);
+
+    render(<App />);
+    const IntroPage = screen.getByText("Welcome to the Trivia Challenge!");
+    expect(IntroPage).toBeInTheDocument();
+  });
 });
