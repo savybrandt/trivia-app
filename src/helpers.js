a944ecb605a5ef9d45c51b650240da2cede9dc33
Savy Brandt <savybrandt@Savys-Air.attlocal.net>
2021-09-10 12:26:32 -0400

    add: first flow + linter updates

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,23 @@
+/* @flow */
 /* eslint-disable import/prefer-default-export */
 import { decode } from 'he';
 
-export const reduceQuestionsPayload = (questions) => (
-  questions.map((question) => ({
-    category: question.category,
-    answer: question.correct_answer,
-    question: decode(question.question),
-  }))
-);
+function reduceQuestionsPayload(questions: Array<{
+  category: string,
+  correct_answer: string,
+  question: string
+}>): Array<{
+  category: string,
+  answer: string,
+  question: string
+}> {
+  return (
+    questions.map((question) => ({
+      category: question.category,
+      answer: question.correct_answer,
+      question: decode(question.question),
+    }))
+  );
+}
+
+export { reduceQuestionsPayload };
