ed9039a25bc7841377bc3b0943942db2ad456ade
Savy Brandt <savybrandt@Savys-Air.attlocal.net>
2021-09-10 13:05:12 -0400

    updt: flow check IntroPage

--- src/components/pages/intro/IntroPage.jsx
+++ src/components/pages/intro/IntroPage.jsx
@@ -1,14 +1,18 @@
+/* @flow */
 import React from 'react';
+import type { Node } from 'react';
 import { Link } from 'react-router-dom';
 import './IntroPage.css';
 
-const IntroPage = () => (
-  <div id="IntroPage">
-    <h1>Welcome to the Trivia Challenge!</h1>
-    <p className="introText">You will be presented with 10 True or False Questions.</p>
-    <p className="introText">Can you score 100%?</p>
-    <Link className="footer-link" to="questions">BEGIN</Link>
-  </div>
-);
+function IntroPage(): Node {
+  return (
+    <div id="IntroPage">
+      <h1>Welcome to the Trivia Challenge!</h1>
+      <p className="introText">You will be presented with 10 True or False Questions.</p>
+      <p className="introText">Can you score 100%?</p>
+      <Link className="footer-link" to="questions">BEGIN</Link>
+    </div>
+  );
+}
 
 export default IntroPage;
